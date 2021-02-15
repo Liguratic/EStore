9b188ace538f4a01a7dd06137baed10930aac68e
Maxim <Liguratic@yandex.ru>
2021-02-15 18:22:17 +0300

    Добавьте файлы проекта.

---
+++ Magaz/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Magaz
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtLogin.Text == "123" && txtPass.Password == "123")
+            {
+                ActionWin actionWin = new ActionWin();
+                this.Hide();
+                actionWin.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Логин или пароль введены не верно");
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
